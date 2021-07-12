932e45ebe35ef8c489106cb67d5dff6b75a316fd
Hoa Shen <chewhoashen@gmail.com>
2021-07-12 16:07:17 +0800

    Update pom.xml:

---
+++ src/main/java/Adder.java
@@ -0,0 +1,20 @@
+/**
+ * The Adder class provides adding methods
+ */
+public class Adder {
+    public Adder() {}
+
+    /**
+     * Adds x and y
+     */
+    public int Add(int x, int y) {
+        return x + y;
+    }
+
+    /**
+     * Concatenates x and y
+     */
+    public String Add(String x, String y) {
+        return x + y;
+    }
+}

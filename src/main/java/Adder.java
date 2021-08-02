7f70bcebcc4e076c95bcd972c27c968047de7354
Hoa Shen <chewhoashen@gmail.com>
2021-08-02 10:27:23 +0800

    add javadoc

--- src/main/java/Adder.java
+++ src/main/java/Adder.java
@@ -6,6 +6,10 @@ public class Adder {
 
     /**
      * Adds x and y
+     *
+     * @param x value
+     * @param y value
+     * @return the sum
      */
     public int Add(int x, int y) {
         return x + y;
@@ -13,10 +17,13 @@ public class Adder {
 
     /**
      * Concatenates x and y
+     *
+     * @param x value
+     * @param y value
+     * @return the sum
      */
     public String Add(String x, String y) {
         return x + y;
     }
 }
 
-// COmment

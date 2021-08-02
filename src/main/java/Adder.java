877e723f858e50181a584e875d6d9a9e9a3bde0d
Hoa Shen <chewhoashen@gmail.com>
2021-08-02 11:22:22 +0800

    setup gh-pages

---
+++ src/main/java/Adder.java
@@ -0,0 +1,29 @@
+/**
+ * The Adder class provides adding methods
+ */
+public class Adder {
+    public Adder() {}
+
+    /**
+     * Adds x and y
+     *
+     * @param x value
+     * @param y value
+     * @return the sum
+     */
+    public int Add(int x, int y) {
+        return x + y;
+    }
+
+    /**
+     * Concatenates x and y
+     *
+     * @param x value
+     * @param y value
+     * @return the sum
+     */
+    public String Add(String x, String y) {
+        return x + y;
+    }
+}
+
